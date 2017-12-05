c6586709b89d123c614c6a11e4fc81754db57fb9
Marvin Altemeier <marv.internetz@gmail.com>
2017-12-05 19:22:17 +0100

    Added partial solution for day three

--- daythree/daythree.go
+++ daythree/daythree.go
@@ -1,5 +1,19 @@
 package daythree
 
+import "math"
+
 func CalcSteps(num int) (steps int) {
 	return
+}
+
+func rowLength(num int) (rowLength int) {
+	rowLength = int(math.Ceil(math.Sqrt(float64(num))))
+	if rowLength%2 == 0 {
+		rowLength += 1
+	}
+	return
+}
+
+func circleStep(num int) int {
+	return (num - 1) / 2
 }
--- daythree/daythree_test.go
+++ daythree/daythree_test.go
@@ -39,3 +39,75 @@ func TestCalcSteps1024(t *testing.T) {
 		t.Fatalf("Expected %d but got %d", expected, actual)
 	}
 }
+
+func TestRowLength1(t *testing.T) {
+	actual := rowLength(1)
+	expected := 1
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestRowLength8(t *testing.T) {
+	actual := rowLength(8)
+	expected := 3
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestRowLength23(t *testing.T) {
+	actual := rowLength(23)
+	expected := 5
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestRowLength27(t *testing.T) {
+	actual := rowLength(27)
+	expected := 7
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestCircleStep1(t *testing.T) {
+	actual := circleStep(1)
+	expected := 0
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestCircleStep3(t *testing.T) {
+	actual := circleStep(3)
+	expected := 1
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestCircleStep5(t *testing.T) {
+	actual := circleStep(5)
+	expected := 2
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestCircleStep7(t *testing.T) {
+	actual := circleStep(7)
+	expected := 3
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d", expected, actual)
+	}
+}
